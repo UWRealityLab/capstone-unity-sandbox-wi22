87f438ce048c1830fec32b2157a1e248b8517601
John Akers <johnfakersnorth@gmail.com>
2022-01-26 23:36:11 -0800

    adds photon integration and example networking

--- Capstone Sandbox WI22/Assets/Scripts/ColorSwapper.cs
+++ Capstone Sandbox WI22/Assets/Scripts/ColorSwapper.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class ColorSwapper : MonoBehaviour
 {
@@ -8,17 +9,31 @@ public class ColorSwapper : MonoBehaviour
     public Material hoverMaterial;
 
     private Material initialMaterial;
+    private PhotonView photonView;
 
     private void Start()
     {
         initialMaterial = GetComponent<Renderer>().material;
+        photonView = PhotonView.Get(this);
     }
 
+    public void startNetworkedHoverColor()
+    {
+        photonView.RPC("startHoverColor", RpcTarget.All);
+    }
+
+    [PunRPC]
     public void startHoverColor()
     {
         GetComponent<Renderer>().material = hoverMaterial;
     }
 
+    public void endNetworkedHoverColor()
+    {
+        photonView.RPC("endHoverColor", RpcTarget.All);
+    }
+
+    [PunRPC]
     public void endHoverColor()
     {
         GetComponent<Renderer>().material = initialMaterial;
